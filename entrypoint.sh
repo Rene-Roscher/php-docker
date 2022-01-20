f4032c51384f0e34b86811511bd6541b5e78e9c6
kasimchen <746215017@qq.com>
2022-01-20 22:03:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,6 @@ if [ "${1#-}" != "$1" ]; then
   set -- php-fpm "$@"
 fi
 
-if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ]; then
-
-  # Cache everything
-  php artisan optimize
-  php artisan config:cache
-
-fi
 
 exec "$@" &
 exec nginx
